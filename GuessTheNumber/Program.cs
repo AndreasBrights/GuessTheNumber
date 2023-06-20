50c8a710727af84ca566f570925372b138396aec
Andreas Andersson <Andreas__Andersson@hotmail.se>
2023-06-20 14:55:28 +0200

    Har börjat på en random number generator, utveckla den! Kanske högre nummer och att man kan komma närmare och närmare med hjälp av sina tidigare gissningar.

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -2,9 +2,28 @@
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int guess = -1;
+            Random random = new Random();
+            int rightNumber = random.Next(0, 10);
+
+            while (guess != rightNumber)
+            {
+                Console.WriteLine("Guess the right number! Between 0 - 10.");
+                guess = int.Parse(Console.ReadLine());
+                if (guess == rightNumber)
+                {
+                    Console.WriteLine("You guess the right number!");
+                }
+                else
+                {
+                    Console.WriteLine("Try again!");
+                }
+            }
+            Console.WriteLine($"{guess} was the right number!");
+
         }
     }
 }
