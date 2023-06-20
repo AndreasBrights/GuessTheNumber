73d7bbede120ae20a3e536f4ed60a8616eb12e9d
Ahmad <your.email@example.com>
2023-06-20 15:19:16 +0200

    La till lite olika funktioner, ifall man gissar över talet så skriver det för högt vice versa. la även till antal gissningar.

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -2,27 +2,44 @@
 {
     internal class Program
     {
-        
+
         static void Main(string[] args)
         {
             int guess = -1;
             Random random = new Random();
-            int rightNumber = random.Next(0, 10);
+            int rightNumber = random.Next(0, 100);
+            int gissning = 1;
 
             while (guess != rightNumber)
             {
-                Console.WriteLine("Guess the right number! Between 0 - 10.");
+                Console.WriteLine("Guess the right number! Between 0 - 100.");
                 guess = int.Parse(Console.ReadLine());
-                if (guess == rightNumber)
+
+                if (guess > rightNumber)
                 {
-                    Console.WriteLine("You guess the right number!");
+                    Console.WriteLine("För högt! Testa lite lägre.");
+                    
                 }
-                else
+
+                else if (guess < rightNumber)
                 {
-                    Console.WriteLine("Try again!");
+                    Console.WriteLine("För lågt!Testa lite högre!.");
                 }
+
+                else if (guess == rightNumber)
+                {
+                    Console.WriteLine($"{guess} was the right number!");
+                    Console.WriteLine($"Det tog dig endast{gissning} försök!");
+
+                }
+
+                else if (guess > 100 || guess < 0)
+                {
+                    Console.WriteLine("Numret är antigen under/över 100!!");
+                }
+
+                gissning++;
             }
-            Console.WriteLine($"{guess} was the right number!");
 
         }
     }
